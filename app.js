8c22fed19ea3286640f46b7a1488f8279c212866
Angel Tardaguela <105347598+Natzuka@users.noreply.github.com>
2022-05-14 16:04:47 +0800

    fixed directories

--- app.js
+++ app.js
@@ -1,26 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, ImageBackground, Image} from 'react-native';
+import React from 'react';
+import { StyleSheet, View} from 'react-native';
+import WelcomeScreen from './scripts/screens/welcome-screen';
 
 export default function App() {
   return (
     <View style={styles.container}>
 
-      <ImageBackground 
-          source={require('./assets/testbackground.jpg')}
-          style={styles.backgroundImage}/>
-
-      <View style={styles.logoContainer}>
-
-        <View style={styles.titles}>
-          <Image 
-          source={require('./assets/favicon.png')}
-          style={styles.logoImage}/>
-
-          <Text style={styles.title}>JEEP IT</Text>
-          <Text style={styles.subtitle1}>MAP FOR JEEPS</Text>
-          <Text style={styles.subtitle2}>Jeep App for Commuters</Text>
-        </View>
-      </View>
+      <WelcomeScreen/>
 
       <StatusBar style="auto" />
     </View>
@@ -30,38 +17,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#0FF",
+    backgroundColor: "#FFF",
     alignItems: "center",
     justifyContent: "center",
-  },
-  logoContainer: {
-    width: "50%",
-    height: "50%",
-  },
-  titles: {
-    alignItems: "center"
-  },
-  title: {
-    fontSize: 50,
-    fontWeight: "bold",
-  },
-  subtitle1: {
-    fontSize: 30,
-    fontWeight: "500",
-  },
-  subtitle2: {
-    fontSize: 30,
-    fontWeight: "300"
-  },
-  backgroundImage: {
-    width: "100%",
-    height: "100%",
-    resizeMode: "cover",
-    position: "absolute",
-  },
-  logoImage: {
-    width: "25%",
-    height: "25%",
-    resizeMode: "cover",
   }
 });
---
+++ scripts/styles.js
@@ -0,0 +1,37 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+    logoContainer: {
+        width: "100%",
+        height: "100%",
+      },
+      titles: {
+        marginTop: "50%",
+        alignItems: "center"
+      },
+      title: {
+        fontSize: 48,
+        fontWeight: "bold",
+      },
+      subtitle1: {
+        fontSize: 18,
+        fontWeight: "500",
+      },
+      subtitle2: {
+        fontSize: 18,
+        fontWeight: "300"
+      },
+      backgroundImage: {
+        width: "100%",
+        height: "100%",
+        resizeMode: "cover",
+        position: "absolute",
+      },
+      logoImage: {
+        width: "25%",
+        height: "25%",
+        resizeMode: "cover",
+      }
+})
+
+export default styles
