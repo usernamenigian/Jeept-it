bfd4bd9263290b81cd7d468ce8add31671fa4027
Angel Tardaguela <105347598+Natzuka@users.noreply.github.com>
2022-05-15 15:56:24 +0800

    added a button

---
+++ scripts/button/ButtonStyles.js
@@ -0,0 +1,18 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+    container: {
+        width: '100%',
+        padding: 10,
+    },
+    button: {
+        borderStartColor: '#FFF',
+        height: 50,
+        borderRadius: 10,
+        justifyContent: "center",
+        alignItems: "center"
+    },
+    text: {
+        fontSize: 40,
+    }
+})
