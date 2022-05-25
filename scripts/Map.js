d1bb62f79c32115fe2ce5d44d0f4be7c24483559
Yoshifumi12 <yoshifumisuzuki81194@gmail.com>
2022-05-25 15:18:49 +0800

    Map pins

--- scripts/Map.js
+++ scripts/Map.js
@@ -92,7 +92,357 @@ const Map =
     pinColor = {"red"}
     title = {"SM Delgado"}
     />
+    
+    
+    <MapView.Marker
+    coordinate = {{latitude: 10.688,
+    longitude: 122.51618}}
+    pinColor = {"red"}
+    title = {"Villa Plaza"}
+    />
+  <MapView.Marker
+    coordinate = {{latitude: 10.6872591,
+    longitude: 122.5192125}}
+    pinColor = {"red"}
+    title = {"Quezon Barangay Hall"}
+    />
+   <MapView.Marker
+    coordinate = {{latitude: 10.6970243,
+    longitude: 122.5429724}}
+    pinColor = {"red"}
+    title = {"Jollibee"}
+    />
+   <MapView.Marker
+    coordinate = {{latitude: 10.6969435,
+    longitude: 122.5441128}}
+    pinColor = {"red"}
+    title = {"Molo Plaza"}
+    />
+       <MapView.Marker
+    coordinate = {{latitude: 10.6960269,
+    longitude: 122.5446206}}
+    pinColor = {"red"}
+    title = {"Mercury Drugstore"}
+    />
+   <MapView.Marker
+    coordinate = {{latitude: 10.6971039,
+    longitude: 122.5471934}}
+    pinColor = {"red"}
+    title = {"Molo Public Cemetery"}
+    />
+   <MapView.Marker
+    coordinate = {{latitude: 10.6977383,
+    longitude: 122.5495828}}
+    pinColor = {"red"}
+    title = {"Iloilo City National Highschool"}
+    />
+   <MapView.Marker
+    coordinate = {{latitude: 10.7021203,
+    longitude: 122.5657076}}
+    pinColor = {"red"}
+    title = {"Assumption Iloilo"}
+    />
+   <MapView.Marker
+    coordinate = {{latitude: 10.7006106,
+    longitude: 122.5682697}}
+    pinColor = {"red"}
+    title = {"The Atrium"}
+    />
+   <MapView.Marker
+    coordinate = {{latitude: 10.6989775,
+    longitude: 122.5688692}}
+    pinColor = {"red"}
+    title = {"Landbank"}
+    />
+       <MapView.Marker
+    coordinate = {{latitude: 10.6984378,
+    longitude: 122.5692699}}
+    pinColor = {"red"}
+    title = {"Chowking"}
+    />
+       <MapView.Marker
+    coordinate = {{latitude: 10.696622,
+    longitude: 122.5686437}}
+    pinColor = {"red"}
+    title = {"Unitop"}
+    />
+   <MapView.Marker
+    coordinate = {{latitude: 10.697044,
+    longitude: 122.5674641}}
+    pinColor = {"red"}
+    title = {"Mary Mart"}
+    />
+   <MapView.Marker
+    coordinate = {{latitude: 10.6986532,
+    longitude: 122.5671546}}
+    pinColor = {"red"}
+    title = {"SM Delgado"}
+    />
+
+
+<MapView.Marker
+    coordinate = {{latitude: 10.774,
+    longitude: 122.574638}}
+    pinColor = {"red"}
+    title = {"Tagbak Bus Terminal"}
+    />
+       <MapView.Marker
+    coordinate = {{latitude: 10.7445817,
+    longitude: 122.56584}}
+    pinColor = {"red"}
+    title = {"Toyota"}
+    />
+           <MapView.Marker
+    coordinate = {{latitude: 10.7410528,
+    longitude: 122.5634871}}
+    pinColor = {"red"}
+    title = {"Jollibee"}
+   />
+  <MapView.Marker
+    coordinate = {{latitude: 10.7244118,
+    longitude: 122.5573263}}
+    pinColor = {"red"}
+    title = {"Jaro Plaza"}
+    />
+   <MapView.Marker
+    coordinate = {{latitude: 10.7190179,
+    longitude: 122.5608054}}
+    pinColor = {"red"}
+    title = {"Jollibee"}
+    />
+   <MapView.Marker
+    coordinate = {{latitude: 10.7135681,
+    longitude: 122.5624973}}
+    pinColor = {"red"}
+    title = {"West Visayas State University"}
+    />
+       <MapView.Marker
+    coordinate = {{latitude: 10.7068854,
+    longitude: 122.5666009}}
+    pinColor = {"red"}
+    title = {"Gaisano Capital City"}
+    />
+   <MapView.Marker
+    coordinate = {{latitude: 10.7006106,
+    longitude: 122.5682697}}
+    pinColor = {"red"}
+    title = {"The Atrium"}
+    />
+   <MapView.Marker
+    coordinate = {{latitude: 10.6994123,
+    longitude: 122.5630039}}
+    pinColor = {"red"}
+    title = {"University of San Agustin"}
+    />
+       <MapView.Marker
+    coordinate = {{latitude: 10.6986532,
+    longitude: 122.5671546}}
+    pinColor = {"red"}
+    title = {"SM Delgado"}
+    />
+   <MapView.Marker
+    coordinate = {{latitude: 10.706645519257266,
+    longitude: 122.56926365208066}}
+    pinColor = {"red"}
+    title = {"Rizal Lapaz Barangay Hall"}
+    />
+   <MapView.Marker
+    coordinate = {{latitude: 10.70260509701925,
+    longitude: 122.56837876372863}}
+    pinColor = {"red"}
+    title = {"Iloilo Provincial Capitol"}
+    />
+
+
+<MapView.Marker
+    coordinate = {{latitude: 10.7310066,
+    longitude: 122.5491166}}
+    pinColor = {"red"}
+    title = {"Central Philippine University"}
+    />
+  <MapView.Marker
+    coordinate = {{latitude: 10.7244118,
+    longitude: 122.5573263}}
+    pinColor = {"red"}
+    title = {"Jaro Plaza"}
+    />
+   <MapView.Marker
+    coordinate = {{latitude: 10.7190179,
+    longitude: 122.5608054}}
+    pinColor = {"red"}
+    title = {"Jollibee"}
+    />
+   <MapView.Marker
+    coordinate = {{latitude: 10.7135681,
+    longitude: 122.5624973}}
+    pinColor = {"red"}
+    title = {"West Visayas State University"}
+    />
+       <MapView.Marker
+    coordinate = {{latitude: 10.7068854,
+    longitude: 122.5666009}}
+    pinColor = {"red"}
+    title = {"Gaisano Capital City"}
+    />
+   <MapView.Marker
+    coordinate = {{latitude: 10.7006106,
+    longitude: 122.5682697}}
+    pinColor = {"red"}
+    title = {"The Atrium"}
+    />
+   <MapView.Marker
+    coordinate = {{latitude: 10.6994123,
+    longitude: 122.5630039}}
+    pinColor = {"red"}
+    title = {"University of San Agustin"}
+    />
+       <MapView.Marker
+    coordinate = {{latitude: 10.6941958,
+    longitude: 122.565573}}
+    pinColor = {"red"}
+    title = {"Robinsons Place"}    
+    />
+           <MapView.Marker
+    coordinate = {{latitude: 10.6926913,
+    longitude: 122.5695755}}
+    pinColor = {"red"}
+    title = {"Iloilo City Central Public Market"}    
+    />
+       <MapView.Marker
+    coordinate = {{latitude: 10.6937917,
+    longitude: 122.5719355}}
+    pinColor = {"red"}
+    title = {"Sunburst Park"}    
+    />
+       <MapView.Marker
+    coordinate = {{latitude: 10.7015547,
+    longitude: 122.5690689}}
+    pinColor = {"red"}
+    title = {"Arroyo Fountain"}
+    />
+
+
+
+<MapView.Marker
+    coordinate = {{latitude: 10.7457002,
+    longitude: 122.5408506}}
+    pinColor = {"red"}
+    title = {"Christ the King Memorial Park"}
+    />
+   <MapView.Marker
+    coordinate = {{latitude: 10.7310066,
+    longitude: 122.5491166}}
+    pinColor = {"red"}
+    title = {"Central Philippine University"}
+    />
+  <MapView.Marker
+    coordinate = {{latitude: 10.7244118,
+    longitude: 122.5573263}}
+    pinColor = {"red"}
+    title = {"Jaro Plaza"}
+    />
+   <MapView.Marker
+    coordinate = {{latitude: 10.7190179,
+    longitude: 122.5608054}}
+    pinColor = {"red"}
+    title = {"Jollibee"}
+    />
+   <MapView.Marker
+    coordinate = {{latitude: 10.7135681,
+    longitude: 122.5624973}}
+    pinColor = {"red"}
+    title = {"West Visayas State University"}
+    />
+       <MapView.Marker
+    coordinate = {{latitude: 10.7068854,
+    longitude: 122.5666009}}
+    pinColor = {"red"}
+    title = {"Gaisano Capital City"}
+    />
+   <MapView.Marker
+    coordinate = {{latitude: 10.7015547,
+    longitude: 122.5690689}}
+    pinColor = {"red"}
+    title = {"Arroyo Fountain"}
+    />
+   <MapView.Marker
+    coordinate = {{latitude: 10.6967916,
+    longitude: 122.5689268}}
+    pinColor = {"red"}
+    title = {"Jollibee"}
+    />
+   <MapView.Marker
+    coordinate = {{latitude: 10.693033218383789,
+    longitude: 122.572509765625}}
+    pinColor = {"red"}
+    title = {"Museum of Philippine Economic History"}
+    />
+   <MapView.Marker
+    coordinate = {{latitude: 10.6926236,
+    longitude: 122.573675}}
+    pinColor = {"red"}
+    title = {"Plaza Libertad"}
+    />
+
+
+
+<MapView.Marker
+    coordinate = {{latitude: 10.7310066,
+    longitude: 122.5491166}}
+    pinColor = {"red"}
+    title = {"Central Philippine University"}
+    />
+  <MapView.Marker
+    coordinate = {{latitude: 10.7244118,
+    longitude: 122.5573263}}
+    pinColor = {"red"}
+    title = {"Jaro Plaza"}
+    />
+   <MapView.Marker
+    coordinate = {{latitude: 10.7190179,
+    longitude: 122.5608054}}
+    pinColor = {"red"}
+    title = {"Jollibee"}
+    />
+   <MapView.Marker
+    coordinate = {{latitude: 10.7135681,
+    longitude: 122.5624973}}
+    pinColor = {"red"}
+    title = {"West Visayas State University"}
+    />
+       <MapView.Marker
+    coordinate = {{latitude: 10.7068854,
+    longitude: 122.5666009}}
+    pinColor = {"red"}
+    title = {"Gaisano Capital City"}
+    />
+   <MapView.Marker
+    coordinate = {{latitude: 10.7015547,
+    longitude: 122.5690689}}
+    pinColor = {"red"}
+    title = {"Arroyo Fountain"}
+    />
+   <MapView.Marker
+    coordinate = {{latitude: 10.6967916,
+    longitude: 122.5689268}}
+    pinColor = {"red"}
+    title = {"Jollibee"}
+    />
+   <MapView.Marker
+    coordinate = {{latitude: 10.693033218383789,
+    longitude: 122.572509765625}}
+    pinColor = {"red"}
+    title = {"Museum of Philippine Economic History"}
+    />
+   <MapView.Marker
+    coordinate = {{latitude: 10.6926236,
+    longitude: 122.573675}}
+    pinColor = {"red"}
+    title = {"Plaza Libertad"}
+    />
+
    </MapView> 
   </View>
 
 export default Map
+
