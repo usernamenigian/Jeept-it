cd06ad65ec88b4d98c5a0442ac2c7926889a0dcf
Angel Tardaguela <105347598+whatever413y@users.noreply.github.com>
2022-05-25 10:43:57 +0800

    Update MapScreen.js

--- scripts/screens/MapScreen.js
+++ scripts/screens/MapScreen.js
@@ -1,15 +1,28 @@
 import React, { Component } from 'react';
-import { ScrollView } from 'react-native';
+import { View, Text } from 'react-native';
 import { Card } from 'react-native-paper';
+import Map from '../Map';
 
 import styles from './styles';
 
 class MapScreen extends Component {
     render () {
         return (
-            <Card>
-                
-            </Card>
+            <View>
+                <Card style={styles.card}>
+                <Card.Title title="Map" />
+                  <Card.Content>
+                  {Map}
+                  </Card.Content>
+                </Card>
+
+                <Card style={styles.card}>
+                 <Card.Title title="Calculation" />
+                  <Card.Content>
+                  <Text>insert here</Text>
+                  </Card.Content>
+                </Card>
+            </View>
         )
     }
     
