ae168f8f3e0ded724bf2f7ae32cc3425e62219f3
Yoshifumi12 <yoshifumisuzuki81194@gmail.com>
2022-05-25 17:43:06 +0800

    maps

---
+++ scripts/jeeps/jaro-cpu.js
@@ -0,0 +1,73 @@
+import MapView, { Marker } from 'react-native-maps';
+import { View } from 'react-native';
+
+import styles from '../screens/styles'
+
+const jaroCPU = 
+  <View style = {styles.container}>
+   <MapView style = {styles.map} 
+    initialRegion = {{
+     latitude:10.7310066,
+     longitude: 122.5491166,
+     latitudeDelta: 0.1,
+     longitudeDelta: 0.045,
+    }}
+   >
+   <MapView.Marker
+    coordinate = {{latitude: 10.7310066,
+    longitude: 122.5491166}}
+    pinColor = {"red"}
+    title = {"Central Philippine University"}
+    />
+  <MapView.Marker
+    coordinate = {{latitude: 10.7244118,
+    longitude: 122.5573263}}
+    pinColor = {"red"}
+    title = {"Jaro Plaza"}
+    />
+   <MapView.Marker
+    coordinate = {{latitude: 10.7190179,
+    longitude: 122.5608054}}
+    pinColor = {"red"}
+    title = {"Jollibee"}
+    />
+   <MapView.Marker
+    coordinate = {{latitude: 10.7135681,
+    longitude: 122.5624973}}
+    pinColor = {"red"}
+    title = {"West Visayas State University"}
+    />
+       <MapView.Marker
+    coordinate = {{latitude: 10.7068854,
+    longitude: 122.5666009}}
+    pinColor = {"red"}
+    title = {"Gaisano Capital City"}
+    />
+   <MapView.Marker
+    coordinate = {{latitude: 10.7015547,
+    longitude: 122.5690689}}
+    pinColor = {"red"}
+    title = {"Arroyo Fountain"}
+    />
+   <MapView.Marker
+    coordinate = {{latitude: 10.6967916,
+    longitude: 122.5689268}}
+    pinColor = {"red"}
+    title = {"Jollibee"}
+    />
+   <MapView.Marker
+    coordinate = {{latitude: 10.693033218383789,
+    longitude: 122.572509765625}}
+    pinColor = {"red"}
+    title = {"Museum of Philippine Economic History"}
+    />
+   <MapView.Marker
+    coordinate = {{latitude: 10.6926236,
+    longitude: 122.573675}}
+    pinColor = {"red"}
+    title = {"Plaza Libertad"}
+    />
+   </MapView> 
+  </View>
+
+export default jaroCPU
---
+++ scripts/jeeps/jaro-liko-tagbak.js
@@ -0,0 +1,92 @@
+import MapView, { Marker } from 'react-native-maps';
+import { View } from 'react-native';
+
+import styles from '../screens/styles'
+
+
+const jaroLikoTagbak =
+<View style = {styles.container}>
+<MapView style = {styles.map} 
+ initialRegion = {{
+  latitude:10.7617375,
+  longitude: 122.5769638,
+  latitudeDelta: 0.1,
+  longitudeDelta: 0.045,
+ }}
+>
+   <MapView.Marker
+ coordinate = {{latitude: 10.774,
+ longitude: 122.574638}}
+ pinColor = {"red"}
+ title = {"Tagbak Bus Terminal"}
+ />
+    <MapView.Marker
+ coordinate = {{latitude: 10.7445817,
+ longitude: 122.56584}}
+ pinColor = {"red"}
+ title = {"Toyota"}
+ />
+        <MapView.Marker
+ coordinate = {{latitude: 10.7410528,
+ longitude: 122.5634871}}
+ pinColor = {"red"}
+ title = {"Jollibee"}
+/>
+<MapView.Marker
+ coordinate = {{latitude: 10.7244118,
+ longitude: 122.5573263}}
+ pinColor = {"red"}
+ title = {"Jaro Plaza"}
+ />
+<MapView.Marker
+ coordinate = {{latitude: 10.7190179,
+ longitude: 122.5608054}}
+ pinColor = {"red"}
+ title = {"Jollibee"}
+ />
+<MapView.Marker
+ coordinate = {{latitude: 10.7135681,
+ longitude: 122.5624973}}
+ pinColor = {"red"}
+ title = {"West Visayas State University"}
+ />
+    <MapView.Marker
+ coordinate = {{latitude: 10.7068854,
+ longitude: 122.5666009}}
+ pinColor = {"red"}
+ title = {"Gaisano Capital City"}
+ />
+<MapView.Marker
+ coordinate = {{latitude: 10.7006106,
+ longitude: 122.5682697}}
+ pinColor = {"red"}
+ title = {"The Atrium"}
+ />
+<MapView.Marker
+ coordinate = {{latitude: 10.6994123,
+ longitude: 122.5630039}}
+ pinColor = {"red"}
+ title = {"University of San Agustin"}
+ />
+    <MapView.Marker
+ coordinate = {{latitude: 10.6986532,
+ longitude: 122.5671546}}
+ pinColor = {"red"}
+ title = {"SM Delgado"}
+ />
+<MapView.Marker
+ coordinate = {{latitude: 10.706645519257266,
+ longitude: 122.56926365208066}}
+ pinColor = {"red"}
+ title = {"Rizal Lapaz Barangay Hall"}
+ />
+<MapView.Marker
+ coordinate = {{latitude: 10.70260509701925,
+ longitude: 122.56837876372863}}
+ pinColor = {"red"}
+ title = {"Iloilo Provincial Capitol"}
+ />
+   </MapView> 
+  </View>
+
+export default jaroLikoTagbak
