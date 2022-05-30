547203a24fd4c18f35e710adeac70bb2720e4096
Angel Tardaguela <105347598+whatever413y@users.noreply.github.com>
2022-05-30 10:07:53 +0800

    changed homescreen function to class

--- scripts/screens/HomeScreen.js
+++ scripts/screens/HomeScreen.js
@@ -1,27 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { View } from 'react-native';
 import { Button, Card } from 'react-native-paper';
 
 import styles from './styles';
 
-function HomeScreen ({ navigation }) {
-  return (
-    <View>
+class HomeScreen extends Component {
+  render () {
+    return (
+     <View>
       <Card style={styles.card}>
         <Card.Title title="Jeepney Fare Guide" />
         <Card.Content>
-          <Button mode="contained" onPress={() => navigation.navigate('Fare List')}>View</Button>
+         <Button mode="contained" onPress={() => this.props.navigation.navigate('Fare List')}>View</Button>
         </Card.Content>
       </Card>
 
       <Card style={styles.card}>
         <Card.Title title="Jeepney List" />
         <Card.Content>
-          <Button mode="contained" onPress={() => navigation.navigate('Jeep List')}>View</Button>
+          <Button mode="contained" onPress={() => this.props.navigation.navigate('Jeep List')}>View</Button>
         </Card.Content>
       </Card>
       
-    </View>
-  )
+     </View>
+    )
+  }
 }
 export default HomeScreen
