3614d736c0f3582fc62d7332986d1e3e7ff00e6a
Gian <97008002+usernamenigian@users.noreply.github.com>
2022-06-01 03:10:41 +0800

    added a jeep files
    dugang2 a

---
+++ scripts/jeeps/oton-derecho.js
@@ -0,0 +1,75 @@
+import MapView, { Marker } from 'react-native-maps';
+import { View } from 'react-native';
+import React from 'react';
+import MapViewDirections from 'react-native-maps-directions';
+
+import styles from '../screens/styles'
+
+const otonDerecho = 
+<View style = {styles.mapContainer}>
+<MapView style = {styles.map}
+initialRegion = {{
+    latitude: 10.708838,
+    longitude: 122.551744,
+    latitudeDelta: 0.1,
+    longitudeDelta: 0.045,
+   }}>
+
+<MapView.Marker
+coordinate = {{latitude: 10.705882,
+    longitude: 122.552442}}
+    pinColor = {"red"}
+    title = {"Small Ville"}
+/>
+<MapView.Marker
+coordinate = {{latitude: 10.703346,
+    longitude: 122.553135}}
+    pinColor = {"red"}
+    title = {"Esplanade"}
+/>
+<MapView.Marker
+coordinate = {{latitude: 10.699881,
+    longitude: 122.553949}}
+    pinColor = {"red"}
+    title = {"Maxx's Stoplight"}
+/>
+<MapView.Marker
+coordinate = {{latitude: 10.718063,
+    longitude: 122.541748}}
+    pinColor = {"red"}
+    title = {"West Visayas Medical Center"}
+/>
+<MapView.Marker
+coordinate = {{latitude: 10.698880,
+    longitude: 122.547189}}
+    pinColor = {"red"}
+    title = {"Molo Terminal"}
+/>
+<MapView.Marker
+coordinate = {{latitude: 10.697205,
+    longitude: 122.542973}}
+    pinColor = {"red"}
+    title = {"Shell Molo"}
+/>
+<MapView.Marker
+coordinate = {{latitude: 10.688931,
+    longitude: 122.516236}}
+    pinColor = {"red"}
+    title = {"Villa Arevalo District Plaza"}
+/>
+<MapView.Marker
+coordinate = {{latitude: 10.693370,
+    longitude: 122.499812}}
+    pinColor = {"red"}
+    title = {"Mohon Terminal"}
+/>
+<MapView.Marker
+coordinate = {{latitude: 10.692254,
+    longitude: 122.478324}}
+    pinColor = {"red"}
+    title = {"Oton Plaza"}
+/>
+</MapView>
+</View>
+
+export default otonDerecho
---
+++ scripts/jeeps/santa-barbara-hi-way.js
@@ -0,0 +1,63 @@
+import MapView, { Marker } from 'react-native-maps';
+import { View } from 'react-native';
+import React from 'react';
+import MapViewDirections from 'react-native-maps-directions';
+
+import styles from '../screens/styles'
+
+const staBarbaraHiWay = 
+<View style = {styles.mapContainer}>
+<MapView style = {styles.map}
+initialRegion = {{
+    latitude: 10.722304,
+    longitude: 122.554899,
+    latitudeDelta: 0.1,
+    longitudeDelta: 0.045,
+   }}>
+
+<MapView.Marker
+coordinate = {{latitude: 10.725028,
+    longitude: 122.549731}}
+    pinColor = {"red"}
+    title = {"Carlos Bakeshop"}
+/>
+<MapView.Marker
+coordinate = {{latitude: 10.718549,
+    longitude: 122.546994}}
+    pinColor = {"red"}
+    title = {"Jolibee Festive"}
+/>
+<MapView.Marker
+coordinate = {{latitude: 10.718177,
+    longitude: 122.542674}}
+    pinColor = {"red"}
+    title = {"Iloilo Supermart - Manduriao"}
+/>
+<MapView.Marker
+coordinate = {{latitude: 10.718063,
+    longitude: 122.541748}}
+    pinColor = {"red"}
+    title = {"West Visayas Medical Center"}
+/>
+<MapView.Marker
+coordinate = {{latitude: 10.717873,
+    longitude: 122.537422}}
+    pinColor = {"red"}
+    title = {"Manduriao Plaza"}
+/>
+<MapView.Marker
+coordinate = {{latitude: 10.725809,
+    longitude: 122.528350}}
+    pinColor = {"red"}
+    title = {"Ana Ros Village"}
+/>
+<MapView.Marker
+coordinate = {{latitude: 10.740320,
+    longitude: 122.517204}}
+    pinColor = {"red"}
+    title = {"Hibao-an Terminal"}
+/>
+</MapView>
+</View>
+
+export default staBarbaraHiWay
