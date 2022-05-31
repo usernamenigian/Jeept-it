e2135fe8bb7174acad8ca2e263891ec95989b5fd
Abril <97876070+Heasy47@users.noreply.github.com>
2022-05-31 10:06:00 -0700

    Update leganes-lapaz.js

--- scripts/jeeps/leganes-lapaz.js
+++ scripts/jeeps/leganes-lapaz.js
@@ -15,11 +15,33 @@ const leganesLapaz =
      longitudeDelta: 0.045,
     }}
    >
+     <MapViewDirections
+    origin = {{latitude: 10.786766, longitude: 122.589266}}
+    waypoints = {[{latitude: 10.784386, longitude: 122.587710},
+                  {latitude: 10.781765, longitude: 122.586120}, 
+                  {latitude: 10.773166, longitude: 122.584567}, 
+                  {latitude: 10.759631, longitude: 122.576240}, 
+                  {latitude: 10.730057, longitude: 122.559084}, 
+                  {latitude: 10.727462, longitude: 122.555608}, 
+                  {latitude: 10.725539, longitude: 122.557130}, 
+                  {latitude: 10.724701, longitude: 122.556367}, 
+                  {latitude: 10.723296, longitude: 122.557597}, 
+                  {latitude: 10.724014, longitude: 122.558360}, 
+                  {latitude: 10.717419, longitude: 122.565080}, 
+                  {latitude: 10.710498, longitude: 122.570366}, 
+                  {latitude: 10.711100, longitude: 122.571230}]}
+    destination = {{latitude: 10.711089, longitude: 122.571241}}
+    apikey = {'AIzaSyDz4sKzFahw8gq6f045fSJ6_xTiIhX2x78'}
+    strokeWidth = {4}
+    strokeColor = "red"
+    optimizeWaypoints={true}
+   />
+
    <MapView.Marker
-    coordinate = {{latitude: 10.785772,
-    longitude: 122.588824}}
+    coordinate = {{latitude: 10.786766,
+    longitude: 122.589266}}
     pinColor = {"red"}
-    title = {"Leganes Town Plaza"}
+    title = {"Leganes Commercial Complex"}
     />
    <MapView.Marker
     coordinate = {{latitude: 10.772256,
@@ -40,10 +62,16 @@ const leganesLapaz =
     title = {"Ceres Northbound Terminal Iloilo City"}
     />
    <MapView.Marker
-    coordinate = {{latitude: 10.734997,
-    longitude: 122.565291}}
+    coordinate = {{latitude: 10.744049,
+    longitude: 122.565313}}
     pinColor = {"red"}
-    title = {"WaterWorld Iloilo"}
+    title = {"Toyota Iloilo, Inc."}
+    />
+   <MapView.Marker
+    coordinate = {{latitude: 10.736712,
+    longitude: 122.561254}}
+    pinColor = {"red"}
+    title = {"Bankers Village IV"}
     />
    <MapView.Marker
     coordinate = {{latitude: 10.730864,
