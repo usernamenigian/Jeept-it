47fa9625e723a8a299f98fa60f590456b3ffeecf
Gian <97008002+usernamenigian@users.noreply.github.com>
2022-06-01 14:26:29 +0800

    oton jeeps updated
    anhawan and derecho

--- scripts/jeeps/oton-anhawan.js
+++ scripts/jeeps/oton-anhawan.js
@@ -15,6 +15,22 @@ initialRegion = {{
     longitudeDelta: 0.045,
    }}>
 
+<MapViewDirections
+    origin = {{latitude: 10.708838, longitude: 122.551744}}
+    waypoints = {[{latitude: 10.705882, longitude: 122.552442},
+                  {latitude: 10.703346, longitude: 122.553135},
+                  {latitude: 10.699881, longitude: 122.553949},
+                  {latitude: 10.698880, longitude: 122.547189},
+                  {latitude: 10.697205, longitude: 122.542973},
+                  {latitude: 10.688931, longitude: 122.516236},
+                  {latitude: 10.693370, longitude: 122.499812}]}
+    destination = {{latitude: 10.692254, longitude: 122.478324}}
+    apikey = {'AIzaSyDz4sKzFahw8gq6f045fSJ6_xTiIhX2x78'}
+    strokeWidth = {4}
+    strokeColor = "red"
+    optimizeWaypoints={false}
+   />
+
 <MapView.Marker
 coordinate = {{latitude: 10.705882,
     longitude: 122.552442}}
@@ -32,12 +48,6 @@ coordinate = {{latitude: 10.699881,
     longitude: 122.553949}}
     pinColor = {"red"}
     title = {"Maxx's Stoplight"}
-/>
-<MapView.Marker
-coordinate = {{latitude: 10.718063,
-    longitude: 122.541748}}
-    pinColor = {"red"}
-    title = {"West Visayas Medical Center"}
 />
 <MapView.Marker
 coordinate = {{latitude: 10.698880,
--- scripts/jeeps/oton-derecho.js
+++ scripts/jeeps/oton-derecho.js
@@ -14,6 +14,38 @@ initialRegion = {{
     latitudeDelta: 0.1,
     longitudeDelta: 0.045,
    }}>
+       
+<MapViewDirections
+    origin = {{latitude: 10.708838, longitude: 122.551744}}
+    waypoints = {[{latitude: 10.705882, longitude: 122.552442},
+                  {latitude: 10.703346, longitude: 122.553135},
+                  {latitude: 10.699881, longitude: 122.553949},
+                  {latitude: 10.698880, longitude: 122.547189},
+                  {latitude: 10.697205, longitude: 122.542973},
+                  {latitude: 10.688931, longitude: 122.516236},
+                  {latitude: 10.693370, longitude: 122.499812}]}
+    destination = {{latitude: 10.692254, longitude: 122.478324}}
+    apikey = {'AIzaSyDz4sKzFahw8gq6f045fSJ6_xTiIhX2x78'}
+    strokeWidth = {4}
+    strokeColor = "red"
+    optimizeWaypoints={false}
+   />
+
+<MapViewDirections
+    origin = {{latitude: 10.692254, longitude: 122.478324}}
+    waypoints = {[{latitude: 10.693370, longitude: 122.499812},
+                  {latitude: 10.703346, longitude: 122.553135},
+                  {latitude: 10.688931, longitude: 122.516236},
+                  {latitude: 10.697205, longitude: 122.542973},
+                  {latitude: 10.698880, longitude: 122.547189},
+                  {latitude: 10.699881, longitude: 122.553949},
+                  {latitude: 10.705882, longitude: 122.552442}]}
+    destination = {{latitude: 10.708838, longitude: 122.551744}}
+    apikey = {'AIzaSyDz4sKzFahw8gq6f045fSJ6_xTiIhX2x78'}
+    strokeWidth = {4}
+    strokeColor = "red"
+    optimizeWaypoints={false}
+   />
 
 <MapView.Marker
 coordinate = {{latitude: 10.705882,
