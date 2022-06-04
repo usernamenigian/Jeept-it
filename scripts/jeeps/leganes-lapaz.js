ce0a4fc675ba9b6218fd2815e1caa680cf9b04c2
Abril <97876070+Heasy47@users.noreply.github.com>
2022-06-04 10:01:19 -0700

    Update leganes-lapaz.js

--- scripts/jeeps/leganes-lapaz.js
+++ scripts/jeeps/leganes-lapaz.js
@@ -1,20 +1,37 @@
 import React from 'react';
 import MapView, { Marker } from 'react-native-maps';
-import { StyleSheet, View } from 'react-native';
+import { View } from 'react-native';
 import MapViewDirections from 'react-native-maps-directions';
 import styles from '../screens/styles'
 
-const leganesLapaz =
+export const leganesLapazMarkers = 
+[
+  {value: 'Leganes Commercial Complex', latitude: 10.786766, longitude: 122.589266},
+  {value: 'CitiHardware Iloilo - Jaro', latitude: 10.772256, longitude: 122.583538},
+  {value: 'CityMall Tagbak', latitude: 10.763576, longitude: 122.578559},
+  {value: 'Ceres Northbound Terminal Iloilo City', latitude: 10.755068, longitude: 122.572003},
+  {value: 'Toyota Iloilo, Inc.', latitude: 10.744049, longitude: 122.565313},
+  {value: 'Bankers Village IV', latitude: 10.736712, longitude: 122.561254},
+  {value: 'Jollibee Tabuc Suba', latitude: 10.730864, longitude: 122.559506},
+  {value: 'J&T Express - Office', latitude: 10.728221, longitude: 122.556402},
+  {value: 'BDO Iloilo - Jaro Branch', latitude: 10.726243, longitude: 122.556528},
+  {value: 'Jaro Plaza', latitude: 10.724483, longitude: 122.557360},
+  {value: 'SM Hypermarket Ilolilo', latitude: 10.720479, longitude: 122.561148},
+  {value: 'Iloilo Science and Technology University', latitude: 10.715720, longitude: 122.566393},
+  {value: 'Lapaz District Plaza', latitude: 10.711820, longitude: 122.570816}
+]
+
+export const leganesLapaz =
   <View style = {styles.container}>
-   <MapView style = {styles.map} 
-    initialRegion = {{
-     latitude: 10.785772,
-     longitude: 122.588824,
-     latitudeDelta: 0.1,
-     longitudeDelta: 0.045,
+    <MapView style = {styles.map} 
+      initialRegion = {{
+      latitude: 10.785772,
+      longitude: 122.588824,
+      latitudeDelta: 0.1,
+      longitudeDelta: 0.045,
     }}
    >
-     <MapViewDirections
+  <MapViewDirections
     origin = {{latitude: 10.786766, longitude: 122.589266}}
     waypoints = {[{latitude: 10.784386, longitude: 122.587710},
                   {latitude: 10.781765, longitude: 122.586120}, 
@@ -34,87 +51,18 @@ const leganesLapaz =
     strokeWidth = {4}
     strokeColor = "red"
     optimizeWaypoints={true}
-   />
+  />
 
-   <MapView.Marker
-    coordinate = {{latitude: 10.786766,
-    longitude: 122.589266}}
-    pinColor = {"red"}
-    title = {"Leganes Commercial Complex"}
-    />
-   <MapView.Marker
-    coordinate = {{latitude: 10.772256,
-    longitude: 122.583538}}
-    pinColor = {"red"}
-    title = {"CitiHardware Iloilo - Jaro"}
-    />
-   <MapView.Marker
-    coordinate = {{latitude: 10.763576,
-    longitude: 122.578559}}
-    pinColor = {"red"}
-    title = {"CityMall Tagbak"}
-    />
-   <MapView.Marker
-    coordinate = {{latitude: 10.755068,
-    longitude: 122.572003}}
-    pinColor = {"red"}
-    title = {"Ceres Northbound Terminal Iloilo City"}
-    />
-   <MapView.Marker
-    coordinate = {{latitude: 10.744049,
-    longitude: 122.565313}}
-    pinColor = {"red"}
-    title = {"Toyota Iloilo, Inc."}
-    />
-   <MapView.Marker
-    coordinate = {{latitude: 10.736712,
-    longitude: 122.561254}}
-    pinColor = {"red"}
-    title = {"Bankers Village IV"}
-    />
-   <MapView.Marker
-    coordinate = {{latitude: 10.730864,
-    longitude: 122.559506}}
-    pinColor = {"red"}
-    title = {"Jollibee Tabuc Suba"}
-    />
-   <MapView.Marker
-    coordinate = {{latitude: 10.728221,
-    longitude: 122.556402}}
-    pinColor = {"red"}
-    title = {"J&T Express - Office"}
-    />
-   <MapView.Marker
-    coordinate = {{latitude: 10.726243,
-    longitude: 122.556528}}
-    pinColor = {"red"}
-    title = {"BDO Iloilo - Jaro Branch"}
-    />
-   <MapView.Marker
-    coordinate = {{latitude: 10.724483,
-    longitude: 122.557360}}
-    pinColor = {"red"}
-    title = {"Jaro Plaza"}
-    />
-   <MapView.Marker
-    coordinate = {{latitude: 10.720479,
-    longitude: 122.561148}}
-    pinColor = {"red"}
-    title = {"SM Hypermarket Ilolilo"}
-    />
-   <MapView.Marker
-    coordinate = {{latitude: 10.715720,
-    longitude: 122.566393}}
-    pinColor = {"red"}
-    title = {"Iloilo Science and Technology University"}
-    />
-   <MapView.Marker
-    coordinate = {{latitude: 10.711820,
-    longitude: 122.570816}}
-    pinColor = {"red"}
-    title = {"Lapaz District Plaza"}
-    />
+  {leganesLapazMarkers.map((marker, index) => {
+             return (<MapView.Marker
+             coordinate = {{latitude: leganesLapazMarkers[index].latitude,
+             longitude: leganesLapazMarkers[index].longitude}}
+             pinColor = {"red"}
+             title = {leganesLapazMarkers[index].value}
+             /> 
+             )})
+  }
    </MapView> 
   </View>
 
-export default leganesLapaz
+
