d64f18d9928108ec37128a27c5a265fb4622969b
Yoshifumi12 <yoshifumisuzuki81194@gmail.com>
2022-06-06 17:46:48 +0800

    Update jaro-cpu.js
    fixed routes

--- scripts/jeeps/jaro-cpu.js
+++ scripts/jeeps/jaro-cpu.js
@@ -28,35 +28,42 @@ export const jaroCPU =
     latitudeDelta: 0.1,
     longitudeDelta: 0.045,
   }}>
-  <MapViewDirections
-    origin={{latitude: 10.7310066, longitude: 122.5491166}}
-    waypoints={[
-      {latitude: 10.691613003096371, longitude: 122.57402551918958},
-    ]}
-    destination={{
-      latitude: 10.691514696588158,
-      longitude: 122.58258372479348,
-    }}
-    apikey={'AIzaSyDz4sKzFahw8gq6f045fSJ6_xTiIhX2x78'}
-    strokeWidth={4}
-    strokeColor="red"
-    optimizeWaypoints={true}
-  />
+        <MapViewDirections
+          origin={{ latitude: 10.7310066, longitude: 122.5491166 }}
+          waypoints={[
+            { latitude: 10.6967916, longitude: 122.5689268 },
+            { latitude: 10.691613003096371, longitude: 122.57402551918958 }
+          ]}
+          destination={{
+            latitude: 10.691514696588158,
+            longitude: 122.58258372479348,
+          }}
+          apikey={'AIzaSyDz4sKzFahw8gq6f045fSJ6_xTiIhX2x78'}
+          strokeWidth={4}
+          strokeColor="red"
+          optimizeWaypoints={true}
+        />
 
-  <MapViewDirections
-    origin={{latitude: 10.691514696588158, longitude: 122.58258372479348}}
-    waypoints={[
-      {latitude: 10.693619677178722, longitude: 122.579265963030689}
-    ]}
-    destination={{
-      latitude: 10.6926236,
-      longitude: 122.573675,
-    }}
-    apikey={'AIzaSyDz4sKzFahw8gq6f045fSJ6_xTiIhX2x78'}
-    strokeWidth={4}
-    strokeColor="red"
-    optimizeWaypoints={true}
-  />
+<MapViewDirections
+          origin={{ latitude: 10.691514696588158, longitude: 122.58258372479348 }}
+          waypoints={[
+            { latitude: 10.693619677178722, longitude: 122.579265963030689 },
+            { latitude: 10.693583433470499, longitude: 122.57165353956078 },
+            { latitude: 10.705819981510704, longitude: 122.56767161686376 },
+            { latitude: 10.720673590415752, longitude: 122.5594646452043 },
+            { latitude: 10.723888430702711, longitude: 122.55840779610143 },
+            { latitude: 10.72720950846234, longitude: 122.55780936261543 }
+            
+          ]}
+          destination={{
+            latitude: 10.732880454561142, 
+            longitude: 122.54965341320211 
+          }}
+          apikey={'AIzaSyDz4sKzFahw8gq6f045fSJ6_xTiIhX2x78'}
+          strokeWidth={4}
+          strokeColor="red"
+          optimizeWaypoints={true}
+        />
 
   {jaroCPUMarkers.map((marker, index) => {
              return (<MapView.Marker
