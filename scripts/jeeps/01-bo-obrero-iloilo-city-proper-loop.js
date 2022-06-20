288751367b7066f59d1c3bd66b8ef78aaf98ea32
Abril <97876070+Heasy47@users.noreply.github.com>
2022-06-20 05:53:52 -0700

    Update 01-bo-obrero-iloilo-city-proper-loop.js

--- scripts/jeeps/01-bo-obrero-iloilo-city-proper-loop.js
+++ scripts/jeeps/01-bo-obrero-iloilo-city-proper-loop.js
@@ -5,10 +5,9 @@ import MapViewDirections from 'react-native-maps-directions';
 import styles from '../screens/styles'
 
 
-//latitude and longitude not updated yet
 export const barrioObreroMarkers = 
 [
-  {value: 'Barrio Obrero Area', latitude: 10.699611, longitude: 122.587731},
+  {value: 'Barrio Obrero Area', latitude: 10.699883, longitude: 122.587400},
   {value: 'Iloilo-Guimaras Ferry Wharf (FF Cruz, RORO)', latitude: 10.701751, longitude: 122.577301},
   {value: 'Casa Real de Iloilo', latitude: 10.701832, longitude: 122.569129},
   {value: 'YMCA Area', latitude: 10.697474, longitude: 122.568932},
@@ -37,14 +36,26 @@ export const barrioObrero =
      longitudeDelta: 0.045,
    }}>
 
-
-//origins not updated yet
 <MapViewDirections
-    origin = {{latitude: 10.701514, longitude: 122.569248}}
-    waypoints = {[{latitude: 10.702253, longitude: 122.571536},
-                  {latitude: 10.701255, longitude: 122.578912},
-                  {latitude: 10.69558, longitude: 122.58446}]}
-    destination = {{latitude:10.699557, longitude: 122.587749}}
+    origin = {{latitude: 10.699883, longitude: 122.587400}}
+    waypoints = {[{latitude: 10.699616, longitude: 122.587734},
+                  {latitude: 10.697213, longitude: 122.585747},
+                  {latitude: 10.697969, longitude: 122.584801},
+                  {latitude: 10.697270, longitude: 122.583459},
+                  {latitude: 10.698204, longitude: 122.582985},
+                  {latitude: 10.698304, longitude: 122.582237},
+                  {latitude: 10.699597, longitude: 122.581535},
+                  {latitude: 10.702267, longitude: 122.571473},
+                  {latitude: 10.701642, longitude: 122.569089},
+                  {latitude: 10.696603, longitude: 122.569113},
+                  {latitude: 10.693932, longitude: 122.571005},
+                  {latitude: 10.692901, longitude: 122.572638},
+                  {latitude: 10.691799, longitude: 122.573864},
+                  {latitude: 10.691658, longitude: 122.573697},
+                  {latitude: 10.692069, longitude: 122.569957},
+                  {latitude: 10.692155, longitude: 122.569130},
+                  {latitude: 10.693112, longitude: 122.569093}]}
+    destination = {{latitude:10.693747, longitude: 122.569092}}
     apikey = {'AIzaSyDz4sKzFahw8gq6f045fSJ6_xTiIhX2x78'}
     strokeWidth = {4}
     strokeColor = "red"
