5b2d3aac3ba0a44b0112364cb38083328f5f9114
Angel Tardaguela <105347598+whatever413y@users.noreply.github.com>
2022-06-20 18:06:25 +0800

    update

--- scripts/JeepsList.js
+++ scripts/JeepsList.js
@@ -1,24 +1,5 @@
-import {barrioObrero, barrioObreroMarkers} from "./jeeps/barrio-obrero"
-import {bitoonJaroPlaza, bitoonJaroPlazaMarkers} from "./jeeps/bitoon-jaro-plaza"
 import {calaparanCalumpang, calaparanCalumpangMarkers} from "./jeeps/02-calaparan-calumpang-iloilo-city-proper"
-import {jaroCPU, jaroCPUMarkers} from "./jeeps/jaro-cpu"
 import {ungkaCPU, ungkaCPUMarkers} from "./jeeps/03-ungka-iloilo-city-proper-via-cpu"
-import {jaroLikoNFA, jaroLikoNFAMarkers} from "./jeeps/jaro-liko-nfa"
-import {jaroLikoTagbak, jaroLikoTagbakMarkers} from "./jeeps/jaro-liko-tagbak"
-import {lapaz, lapazMarkers} from "./jeeps/lapaz"
-import {lapuz, lapuzMarkers} from "./jeeps/lapuz"
-import {leganesLapaz, leganesLapazMarkers} from "./jeeps/leganes-lapaz"
-import {leganesSM, leganesSMMarkers} from "./jeeps/leganes-sm"
-import {manduriaoMolo, manduriaoMoloMarkers} from "./jeeps/manduriao-molo-jibao-an"
-import {mandurriaoSM, mandurriaoSMMarkers} from "./jeeps/mandurriao-sm"
-import {moloBaluarte, moloBaluarteMarkers} from "./jeeps/molo-baluarte"
-import {moloTimawa, moloTimawaMarkers} from "./jeeps/molo-timawa"
-import {otonAnhawan, otonAnhawanMarkers} from "./jeeps/oton-anhawan"
-import {otonDerecho, otonDerechoMarkers} from "./jeeps/oton-derecho"
-import {parolaSuper, parolaSuperMarkers} from "./jeeps/parola-super"
-import {staBarbaraHiWay, staBarbaraHiWayMarkers} from "./jeeps/santa-barbara-hi-way"
-import {ungkaTanza, ungkaTanzaMarkers} from "./jeeps/ungka-tanza"
-import {villa, villaMarkers} from "./jeeps/villa"
 
 class Jeep {
     constructor(name, map, markers) {
@@ -29,47 +10,9 @@ class Jeep {
 }
 
 const jeeps = []
-const BarrioObrero = new Jeep('Barrio Obrero', barrioObrero, barrioObreroMarkers)
-jeeps.push(BarrioObrero)
-const BitoonJaroPlaza = new Jeep('Bitoon Jaro Plaza', bitoonJaroPlaza, bitoonJaroPlazaMarkers)
-jeeps.push(BitoonJaroPlaza)
 const CalaparanCalumpang = new Jeep('Calaparan Calumpang', calaparanCalumpang, calaparanCalumpangMarkers)
 jeeps.push(CalaparanCalumpang)
-const JaroCPU = new Jeep('Jaro CPU', jaroCPU, jaroCPUMarkers)
-jeeps.push(JaroCPU)
 const UngkaCPU = new Jeep('Ungka via CPU', ungkaCPU, ungkaCPUMarkers)
 jeeps.push(UngkaCPU)
-const JaroLikoNFA = new Jeep('Jaro Liko NFA', jaroLikoNFA, jaroLikoNFAMarkers)
-jeeps.push(JaroLikoNFA)
-const JaroLikoTagbak = new Jeep('Jaro Liko Tagbak', jaroLikoTagbak, jaroLikoTagbakMarkers)
-jeeps.push(JaroLikoTagbak)
-const Lapaz = new Jeep('Lapaz', lapaz, lapazMarkers)
-jeeps.push(Lapaz)
-const Lapuz = new Jeep('Lapuz', lapuz, lapuzMarkers)
-jeeps.push(Lapuz)
-const LeganesLapaz = new Jeep('Leganes Lapaz', leganesLapaz, leganesLapazMarkers)
-jeeps.push(LeganesLapaz)
-const LeganesSM = new Jeep('Leganes SM', leganesSM, leganesSMMarkers)
-jeeps.push(LeganesSM)
-const ManduriaoMolo = new Jeep('Mandurriao Molo', manduriaoMolo, manduriaoMoloMarkers)
-jeeps.push(ManduriaoMolo)
-const MandurriaoSM = new Jeep('Mandurriao SM', mandurriaoSM, mandurriaoSMMarkers)
-jeeps.push(MandurriaoSM)
-const MoloBaluarte = new Jeep('Molo Baluarte', moloBaluarte, moloBaluarteMarkers)
-jeeps.push(MoloBaluarte)
-const MoloTimawa = new Jeep('Molo Timawa', moloTimawa, moloTimawaMarkers)
-jeeps.push(MoloTimawa)
-const OtonAnhawan = new Jeep('Oton Anhawan', otonAnhawan, otonAnhawanMarkers)
-jeeps.push(OtonAnhawan)
-const OtonDerecho = new Jeep('Oton Derecho', otonDerecho, otonDerechoMarkers)
-jeeps.push(OtonDerecho)
-const ParolaSuper = new Jeep('Parola Super', parolaSuper, parolaSuperMarkers)
-jeeps.push(ParolaSuper)
-const StaBarbaraHiWay = new Jeep('Santa Barbara Hi-Way', staBarbaraHiWay, staBarbaraHiWayMarkers)
-jeeps.push(StaBarbaraHiWay)
-const UngkaTanza = new Jeep('Ungka Tanza', ungkaTanza, ungkaTanzaMarkers)
-jeeps.push(UngkaTanza)
-const Villa = new Jeep('Villa', villa, villaMarkers)
-jeeps.push(Villa)
 
 export default jeeps
